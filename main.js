76255addea81c03c733928678498ddfc20846827
williestream <103041029+williestream@users.noreply.github.com>
2022-09-01 14:11:47 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,138 @@
+const Trips = [{
+    id: 1,
+    Destination: "กรุงเทพ",
+    FirstImg: "/image/thamaharaj.png",
+    FirstTittle: "ท่ามหาราช",
+    FirstDes: "หากคุณกำลังมองหาที่พักผ่อนหย่อนใจ เดินเล่นสบายชิว ๆ ในวันหยุดเสาร์-อาทิตย์ หรือหาที่อ่านหนังสือสงบ ๆ ท่ามหาราชคือคอมมูนิตี้มอลล์ที่คุณไมม่ควรพลาด ",
+    SecondImg: "/image/moca-museum.png",
+    SecondTittle: "พิพิธภัณฑ์ศิลปะไทย",
+    SecondDes: "พิพิธภัณฑ์ศิลปะไทยร่วมสมัย ชื่นชมความสวยงามของศิลปะที่มีผลงานอยู่มากถึง 5 ชั้น โดยแต่ละชั้นก็จะมีแนวคิดการจัดแสดงที่แตกต่างกันออกไป ถือว่าเป็นแลนด์มาร์คถ่ายรูปสวย ๆ ที่ต้องไปสักครั้ง",
+    ThirdImg: "/image/george fair.png",
+    ThirdTittle: "ตลาดจ๊อดแฟร์",
+    ThirdDes: "สถานที่สุดคุ้นเคยของนักช้อป และหาของกินเล่น ที่มีทั้งร้านอาหาร คาเฟ่แบบมินิมอล สตรีทฟู๊ดชื่อดัง ของงานคราฟและงานแฮนด์เมดแบบคูลๆ"
+}, 
+
+{
+    id: 2,
+    Destination: "ราชบุรี",
+    FirstImg: "/image/corofield.png",
+    FirstTittle: "โคโรฟิลด์",
+    FirstDes: "คาเฟ่ฉบับฟาร์มญี่ปุ่นแห่งแรกของเมืองไทย ตกแต่งสไตล์มินิมอล และยังมีกิจกรรมให้ทำมากมาย อย่างเช่น การปลูกผัด เก็บผลมะเขือเทศ ตกแต่งต้นไม้ และยังมีร้านอาหารและเครื่องดื่มสูตรลับเฉพาะตัวอีกด้วย",
+    SecondImg: "/image/อุทยานหินเขางู.png",
+    SecondTittle: "อุทยานหินเขางู",
+    SecondDes: "พักผ่อนวันหยุดสบาย ๆ กับธรรมชาติที่สวยงามที่อุทยานหินเขางู สถานที่ท่องเที่ยวโบราณคดี ซึ่งบนภูเขาจะมีถ้ำฤาษี และบริเวณรอบๆก็จะมีฝูงลิงเป็นจำนวนมาก",
+    ThirdImg: "/image/ตลาดน้ำดำเนินสะดวก.png",
+    ThirdTittle: "ตลาดน้ำดำเนินสะดวก",
+    ThirdDes: "ตลาดน้ำที่เก่าแก่นับกว่าร้อยปีของไทยและมีชื่อเสียงไกลถึงต่างประเทศ โดยที่ตลาดแห่งนี้มีสินค้าให้เลือกซื้อ และของกิน ของใช้มากมาย รวมถึงของที่ระลึก โดยจะเปิดให้บริการตั้งแต่ 8.00น. ถึง 13.00น."
+
+},
+
+{
+    id: 3,
+    Destination: "เขาใหญ่",
+    FirstImg: "/image/Animalclub.png",
+    FirstTittle: "My Ozone Animal Club",
+    FirstDes: "สวนสัตว์ภายในรีสอร์ท Movenpick Resort KhaoYai ที่เหมาะมาก ๆ กับการมาพักผ่อนสไตล์ครอบครัว ซึ่งเราจะได้ใกล้ชิดกับสัตว์มากมายมาย ทั้ง ม้า ไก่ กระต่าย แกะ แพะ อีกทั้งยังมีฟาร์มออร์แกนิกอีกด้วย",
+    SecondImg: "/image/PBvalley.png",
+    SecondTittle: "PB Valley",
+    SecondDes: "ไร่องุ่น PB Valley ท่ามกลางหุบเขาที่เราจะได้เห็นทิวทัศน์ที่สวยงาม โดยกิจกรรมภายในไร้องุ่น เจ้าหน้าที่จะอธิบายประวัติความเป็นมา พร้อมเรียนรู้วิธิการหมักไวน์",
+    ThirdImg: "/image/พรีโม.png",
+    ThirdTittle: "พรีโม เพียซซ่า",
+    ThirdDes: "สถานที่ที่รายล้อมไปด้วยสถาปัตยกรรมของหมู่บ้านโบราณอายุกว่า 500 ปี ในแคว้านทีสกานี ประเทศอิตาลี ท่ามกลางบรรยากาศสบาย ๆ และยังมีน้อง ๆ แกะเมอริโม่ อัลปาก้า ดองกี้ ให้เราเล่นอีกด้วย "
+
+},  
+
+{
+    id: 4,
+    Destination: "ชลบุรี",
+    FirstImg: "/image/พิพิธภัณฑ์สัตว์น้ำบางแสน.png",
+    FirstTittle: "พิพิธภัณฑ์สัตว์น้ำบางแสน",
+    FirstDes: "สถาบันวิทยาศาสตร์ทางทะเล ของมหาวิทยาลัยบูรพา ที่เราจะได้พบเจอกับสัตว์ทะเลชนิดต่าง ๆ และความรู้เกี่ยวกับวิทยาศาสตร์ทางทะเล สิ่งมีชีวิต และความเป็นอยู่ของสัตว์ทะเลเหล่านี้",
+    SecondImg: "/image/อ่างเก็บน้ำบางพระ.png",
+    SecondTittle: "อ่างเก็บน้ำบางพระ",
+    SecondDes: "ที่เที่ยวสุดโรแมนติกแห่งหนึ่งของจังหวัดชลบุรี ที่มีมุมสวย ๆ ให้ถ่ายรูป นกหลากหลายชนิดคอยบินเติมเต็มบรรยากาศ และถนนเลียบอ่างเก็บน้ำ ที่เต็มไปด้วยต้นตะแบกสีม่วงเบ่งบานอย่างสวยงาม",
+    ThirdImg: "/image/เกาะล้าน.png",
+    ThirdTittle: "เกาะล้าน",
+    ThirdDes: "อีกเกาะสุดฮิตของทะเลตะวันออกที่ต้องขอบอกว่า จะไปกี่ครั้งก็ไม่เคยเบื่อ เพราะน้ำทะเลที่สวยงามและยังอยู่ไม่ห่างจากตัวเมืองพัทยา และจุดชมวิวในเกาะอีกมากมาย เช่น สะพานไม้หาดสังวาล หาดตาแหวน และจุดชมวิวกังหันลม"
+
+},
+{
+id: 5,
+    Destination: "เชียงใหม่",
+    FirstImg: "/image/ดอยอ่างขาง.png",
+    FirstTittle: "ดอยอ่างขาง",
+    FirstDes: "ดินแดนบนดอยสุดโรแมนติก ที่จะมีอากาศเย็นสบาย รวมถึงชื่นชมแปลงดอกไม้และพืชผักเมืองหนาวอันสวยงาม สัมผัสวิถีวิตชาวไทยภูเขาที่มีความหลากหลาย",
+    SecondImg: "/image/ดอยอินทนนท์.png",
+    SecondTittle: "ดอยอินทนนท์",
+    SecondDes: "ยอดดอยสูงเสียดฟ้าอันดับ 1 ของประเทศไทย ดื่มด่ำกับบรรยากาศที่สวยงามบนจุดสูงสุดของภูเขา และยังขับรถขึ้นไปได้อย่างสะดวกสบาย มีจุดให้แวะเที่ยวตลาดทั้งทาง",
+    ThirdImg: "/image/ม่อนแจ่ม.png",
+    ThirdTittle: "ม่อนแจ่ม",
+    ThirdDes: "ที่เที่ยวยอดฮิตของเชียงใหม่ ที่เรียกได้ว่ามาเชียงใหม่ก็ต้องมาม่อนแจ่ม เพราะมีอากาศเย็นสบายตลอดทั้งปี และคุณจะได้ชมวิวภูเขาสลับซับซ้อนที่สวยงาม และหากไปเที่ยวตอนเช้าก็จะได้เห็นหมอกสีขาวสวยงามมาก"
+},
+
+{
+    id: 6,
+        Destination: "เกาะเต่า",
+        FirstImg: "/image/หาดตาโต๊ะกลาง.png",
+        FirstTittle: "หาดตาโต๊ะกลาง",
+        FirstDes: "หาดตาโต๊ะกลาง หรือที่เรียกอีกอย่างว่า Freedom Beach เป็นอีกหนึ่งชายหาดที่คุ้มค่ามากๆ สำหรับคนที่ชอบทะเล เพราะที่นี้มีหาดทรายสีขาวละเอียด น้ำทะเลใสปิ๊ง และต้นเทียนที่อยู่เรียงราย และมุมถ่ายรูปสวย ๆ อีกเพียบ",
+        SecondImg: "/image/จุดชมวิว จอห์น สุวรรณ.png",
+        SecondTittle: "จุดชมวิว จอห์น สุวรรณ",
+        SecondDes: "จุดชมวิวที่ได้ชื่อมาจากผู้ที่ค้นพบ ซึ่งถึงแม้การเดินทางมาที่จุดชมวิวแห่งนี้จะค่อนข้างลำบากนิสนุงแต่รับรองว่าคุ้มค่าแน่นอน เพราะจะได้ชมทิวทัศน์แบบพาโนรามา 180 องศาน่าตื่นตาตื่นใจมาก ๆ ",
+        ThirdImg: "/image/ดำน้ำ ชมปะการัง.png",
+        ThirdTittle: "ดำน้ำ ชมปะการัง",
+        ThirdDes: "มาเกาะเต่าแล้วไม่มาดำน้ำก็เหมือนมาไม่ถึงเกาะเต่า เพราะที่นี้เป็นแหล่งดำน้ำที่ติดอันดับโลก โดยเมื่อดำลงไปเราจะได้เห็นปะการังสวยงาม และสัตว์น้ำมากมาย"
+    }
+]
+
+const destination = document.querySelector('.Destination');
+const firstImg = document.querySelector('.img-01');
+const firstTittle = document.querySelector('.trips-tittle');
+const firstDes = document.querySelector('.trips-des');
+
+const secondImg = document.querySelector('.img-02');
+const secondTittle = document.querySelector('.trips-tittle-02');
+const secondDes = document.querySelector('.trips-des-02');
+
+const thirdImg = document.querySelector('.img-03');
+const thirdTittle = document.querySelector('.trips-tittle-03');
+const thirdDes = document.querySelector('.trips-des-03');
+
+const randomBtn = document.querySelector('.main-btn');
+
+let currentItem = 0;
+
+window.addEventListener('DOMContentLoaded', function(){
+     showTrips();
+
+})
+
+function showTrips() {
+    const item = Trips[currentItem];
+    destination.textContent = item.Destination;
+    firstImg.src = item.FirstImg;
+    firstTittle.textContent = item.FirstTittle;
+    firstDes.textContent = item.FirstDes;
+
+    secondImg.src = item.SecondImg;
+    secondTittle.textContent = item.SecondTittle;
+    secondDes.textContent = item.SecondDes;
+
+    thirdImg.src = item.ThirdImg;
+    thirdTittle.textContent = item.ThirdTittle;
+    thirdDes.textContent = item.ThirdDes;
+    
+}
+
+
+randomBtn.addEventListener('click', function(){
+    
+    currentItem = Math.floor(Math.random() * Trips.length);
+    console.log(currentItem);
+    showTrips();
+})
+
+
+
+
+
